652638eddf0631c69a0abe7e2e46c05bcebf7100
La Tran Thuy Tien <ltrn.thuytien@gmail.com>
2025-05-20 21:44:20 +0700

    KeNamGiuGiacMo

---
+++ O24_BinhDuong(C2)/B1_KeNamGiuGiacMo.cpp
@@ -0,0 +1,42 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int so_luong_uoc(int n) {
+	int count = 0;
+	for(int j = 1; j <= sqrt(n); ++j) {
+		if(n % j == 0) {
+			if(n / j != j) {
+				count += 2;
+			} else {
+				count += 1;
+			}
+		}
+	}
+	return count;
+}
+
+int main(){
+    ios_base::sync_with_stdio(0);
+    cin.tie(0);
+    cout.tie(0);
+    
+    int T, n, k;
+    cin >> T;
+    
+    while(T--){
+        cin >> n >> k;
+        int arr[n], result, ucln = 0;
+        vector <int> arr1;
+        for(int i = 0; i < n; ++i){
+            cin >> arr[i];
+            if(so_luong_uoc(arr[i]) >= k){
+                arr1.push_back(arr[i]);
+            }
+        }
+        for(int i = 1; i <=arr1.size(); ++i){
+            ucln = gcd(ucln, arr1[i]);
+        }
+        cout << so_luong_uoc(ucln);        
+    }
+
+}
